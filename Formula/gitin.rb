8bd30194bacf873cb18d8ddb081f4a257ce7db51
Jeroen Knoops <jeroen.knoops@gmail.com>
2019-02-05 12:41:30 +0100

    :memo: Docs: Adds homepage

--- Formula/gitin.rb
+++ Formula/gitin.rb
@@ -1,6 +1,6 @@
 class Gitin < Formula
   desc "commit/branch/workdir explorer for git"
-  homepage ""
+  homepage "https://github.com/isacikgoz/gitin"
   url "https://github.com/isacikgoz/gitin/releases/download/v0.1.0/gitin_0.1.0_darwin_amd64.tar.gz"
   sha256 "96cdc4ee7ff25f6afb320b73a88ce8721cdfeea75bb4d65fdf70f361ca904b07"
   depends_on "libgit2"
