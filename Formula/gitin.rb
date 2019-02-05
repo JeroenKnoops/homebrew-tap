fa970ab9ab9fb689298a28b5d350dbdcacbec120
Jeroen Knoops <jeroen.knoops@gmail.com>
2019-02-05 12:30:23 +0100

    :bug: Fix: Adds libgit2 as dependency

--- Formula/gitin.rb
+++ Formula/gitin.rb
@@ -3,6 +3,7 @@ class Gitin < Formula
   homepage ""
   url "https://github.com/isacikgoz/gitin/releases/download/v0.1.0/gitin_0.1.0_darwin_amd64.tar.gz"
   sha256 "96cdc4ee7ff25f6afb320b73a88ce8721cdfeea75bb4d65fdf70f361ca904b07"
+  depends_on "libgit2"
 
   def install
     bin.install "gitin"
